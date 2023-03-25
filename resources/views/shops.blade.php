b401fef16b7e1c92e25408671a25ad8f00b4372a
koyama-anna <anna.koyama.2224@gmail.com>
2023-03-25 19:33:12 +0900

    店舗マイグレーション・シーディング#2

---
+++ resources/views/shops.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>店舗一覧</title>
+</head>
+<body>
+    <div class="ttl">Rese</div>
+    <div class="search">
+        <div class="search-area"></div>
+        <div class="search-genre"></div>
+        <div class="search-search"></div>
+    </div>
+
+    <div class="shop-card">
+        <div class="shop-photo"></div>
+        <div class="shop-name">{{$shop->name}}</div>
+    </div>
+    
+</body>
+</html>
