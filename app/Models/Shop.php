d710794d0a8554a4805d68139c829296e37a7a23
koyama-anna <anna.koyama.2224@gmail.com>
2023-03-26 21:13:16 +0900

    アクセサの追加#3

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
@@ -15,4 +16,22 @@ class Shop extends Model
     }
 
     protected $fillable = ['name', 'area', 'genre', 'information', 'photo'];
+
+    protected $appends = [
+        'favorited_by_user'
+    ];
+
+
+    //アクセサ　favorited_by_user
+
+    public function getFavoritedByUser()
+    {
+        if (Auth::check()) {
+            return false;
+        }
+
+        return $this->favorite->contains(function ($user) {
+            return $user->id === Auth::user()->id;
+        });
+    }
 }
