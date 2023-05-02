12b5511265fa03e753080d5771395d879f1ad98f
gem-yours <urgems000@gmail.com>
2023-05-02 18:27:00 +0000

    ログインAPIの作成 #10

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\ShopInfoController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +19,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+        $token = $user->createToken('token-name')->plainTextToken;
+        return response()->json(['token' => $token]);
+    }
+
+    return response()->json(['error' => 'Unauthorized'], 401);
+});
+
+Route::middleware((['auth:sanctum']))->group(function () {
+    Route::apiResource('/v1/shop', ShopInfoController::class);
+    Route::apiResource('/v1/favorite', FavoriteController::class);
 });
